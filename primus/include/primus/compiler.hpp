2d0ea409bddd4418870c15a7e279f429fcb95e3e
Morgan Funtowicz <funtowiczmo@gmail.com>
2025-08-07 22:29:02 +0200

    feat(llvm): it builds on my machine

--- primus/include/primus/compiler.hpp
+++ primus/include/primus/compiler.hpp
@@ -2,17 +2,24 @@
 #define PRIMUS_COMPILER_HPP
 
 #include <filesystem>
+#include "mlir/IR/ImplicitLocOpBuilder.h"
+#include <mlir/IR/BuiltinOps.h>
 
 namespace primus
 {
     class Compiler {
+    protected:
+        mlir::ModuleOp module;
+
+        explicit Compiler(mlir::ModuleOp module);
+
     public:
         /**
          * Create a compiler instance from a file `input`
-         * @param input File path to read the kernel definition from
+         * @param file File path to read the kernel definition from
          * @return
          */
-        static Compiler FromFile(std::filesystem::path input);
+        static Compiler FromFile(const std::filesystem::path& file);
     };
 }
 #endif // PRIMUS_COMPILER_HPP
--- primus/src/primus/compiler.cpp
+++ primus/src/primus/compiler.cpp
@@ -1,24 +1,54 @@
 #include <filesystem>
 #include <fstream>
 
-#include "spdlog/spdlog.h"
-#include "spdlog/fmt/std.h"
-
 #include "primus/compiler.hpp"
+
+#include "spdlog/fmt/fmt.h"
+#include "spdlog/spdlog.h"
+
+
+#include "llvm/Support/SourceMgr.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/OwningOpRef.h"
+#include "mlir/Parser/Parser.h"
+#include "stablehlo/dialect/Register.h"
 
 namespace primus
 {
-    Compiler Compiler::FromFile(std::filesystem::path input)
+    Compiler::Compiler(const mlir::ModuleOp module):  module(module)
     {
-        if (!std::filesystem::exists(input))
+        if constexpr (IS_DEBUG)
         {
-            SPDLOG_ERROR("{} does not exist", input);
-            throw std::runtime_error("Could not open input file");
+            SPDLOG_DEBUG("Initialized compiler with module:");
+            module->dumpPretty();
         }
-
-        SPDLOG_INFO("Processing kernel from {}", input);
-        auto fin = std::ifstream(input);
-        return Compiler{};
     }
 
+    Compiler Compiler::FromFile(const std::filesystem::path& file)
+    {
+        SPDLOG_INFO("Processing kernel from {}", file.string());
+        llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> fileOrErr =
+            llvm::MemoryBuffer::getFileOrSTDIN(file.string(), true);
+        if (const std::error_code ec = fileOrErr.getError()) {
+            throw std::runtime_error(spdlog::fmt_lib::format("Could not open input file {}: {}", file.string(), ec.message()));
+        }
+
+        // Register dialects
+        mlir::DialectRegistry registry;
+        mlir::stablehlo::registerAllDialects(registry);
+
+        // Parse the input mlir.
+        mlir::MLIRContext context(registry);
+        llvm::SourceMgr sourceMgr;
+
+        sourceMgr.AddNewSourceBuffer(std::move(*fileOrErr), llvm::SMLoc());
+        mlir::OwningOpRef<mlir::ModuleOp> owningModule = mlir::parseSourceFile<mlir::ModuleOp>(sourceMgr, &context);
+        if (!owningModule) {
+            throw std::runtime_error(spdlog::fmt_lib::format("Parsing of input file {} failed", file.string()));
+        }
+
+        return Compiler(owningModule.release());
+    }
 }
